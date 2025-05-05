548dc464faae622b73e42eef0597b20331a82cc3
satoshinakahara <sat19kyuju@gmail.com>
2025-05-06 01:15:13 +0900

    build結果

---
+++ dist/index.js
@@ -0,0 +1,80 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const PORT = 8080;
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+const prisma = new client_1.PrismaClient();
+app.get('/allTodos', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const allTodos = yield prisma.todo.findMany();
+        res.json(allTodos);
+    }
+    catch (error) {
+        console.error('Error fetching todos:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}));
+app.post('/createTodo', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, isCompleted } = req.body;
+    try {
+        const newTodo = yield prisma.todo.create({
+            data: {
+                title,
+                isCompleted,
+            },
+        });
+        res.json(newTodo);
+    }
+    catch (error) {
+        console.error('Error creating todo:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}));
+app.put('/editTodo/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const id = Number(req.params.id);
+        const { title, isCompleted } = req.body;
+        const updatedTodo = yield prisma.todo.update({
+            where: { id },
+            data: {
+                title,
+                isCompleted,
+            },
+        });
+        res.json(updatedTodo);
+    }
+    catch (error) {
+        console.error('Error updating todo:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}));
+app.delete('/deleteTodo/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const id = Number(req.params.id);
+        const deletedTodo = yield prisma.todo.delete({
+            where: { id },
+        });
+        res.json(deletedTodo);
+    }
+    catch (error) {
+        console.error('Error deleting todo:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
